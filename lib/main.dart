edd78ebd213808b188c0a89e9930a60cb2c9332f
Antonio <anlaifel09@gmail.com>
2025-04-21 11:08:54 -0400

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mi_terrenito/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mi Terrenito ',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home:  Text('Mi Terrenito'),
+    );
+  }
+}
+
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int selectedIndex = 0;
+
+  final List<Widget> pages = <Widget>[
+    Text('Lista de Terrenos'),
+    Text('Agregar Terreno'),
+    Text('Detalles de Terreno'),
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          Icon(Icons.person)
+        ],
+      ),
+    
+      body: Center(
+        child: pages.elementAt(selectedIndex),
+      ),     
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Lista',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Agregar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.details),
+            label: 'Detalles de Terreno',
+            )
+        ],
+        ),
+    );
+  }
+
+}
