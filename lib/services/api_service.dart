0e9fbfd8bf8107fe78ede2fc64344e78811ccb07
Antonio <anlaifel09@gmail.com>
2025-04-30 21:57:48 -0400

    Cambios en la url de la api

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,10 +3,10 @@ import 'package:http/http.dart' as http;
 import '../models/property/property.dart';
 
 class ApiService {
-  static const String baseUrl = 'https://api-terrenito.onrender.com/api';
+  static const String baseUrl = 'http://192.168.0.6:8000';
 
   Future<List<Property>> fetchProperties() async {
-    final response = await http.get(Uri.parse('$baseUrl/propiedades'));
+    final response = await http.get(Uri.parse('$baseUrl/properties'));
 
     if (response.statusCode == 200) {
       final List<dynamic> jsonResponse = json.decode(response.body);
