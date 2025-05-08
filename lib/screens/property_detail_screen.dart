efef9df36baf8a374e39b96d20fd3ff9ae37e384
xavi235 <linkshaoran24@gmail.com>
2025-05-07 20:25:59 -0400

    .

--- lib/screens/property_detail_screen.dart
+++ lib/screens/property_detail_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // ← Importante
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../models/property/property.dart';
 
 class PropertyDetailScreen extends StatefulWidget {
