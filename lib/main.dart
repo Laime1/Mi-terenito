9445989d56d5f4f52d4eed6b7f3f6471f001f5d4
Antonio <anlaifel09@gmail.com>
2025-06-04 20:47:53 -0400

    nuevo home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mi_terrenito/screens/home1_screen.dart';
 import 'package:mi_terrenito/screens/home_screen.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
      
-      home: const HomeScreen(),
+      home: const Home1Screen(),
 
 
     );
